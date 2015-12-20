bc64ae8ca176326a2f04724ca30680479c006497
Jason Gardner <jason.gardner@kaseya.com>
2015-12-19 19:26:32 -0800

    use a quick panel to select from avail locations

--- plugins/OpenLocation/openloc.py
+++ plugins/OpenLocation/openloc.py
@@ -1,17 +1,19 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, json
+from pprint import pprint
+
 
 class OpenlocationPromptCommand(sublime_plugin.WindowCommand):
 
     def run(self):
-        self.window.show_input_panel("Location Key:", "", self.on_done, None, None)
-        pass
-
-    def on_done(self, text):
         settings = sublime.load_settings('OpenLocation.sublime-settings')
-        if settings.has(text):
-            path = settings.get(text)
-            sublime.run_command("new_window")
-            d = {'folders': [{'follow_symlinks': True, 'path': path}]}
-            sublime.active_window().set_project_data(d)
-        else:
-            sublime.error_message('Unrecognized location key: ' + text)
+        with open(settings.get('settingsPath')) as json_data:
+            self.settings_json = json.load(json_data)
+            json_data.close()
+            self.aliasKeys = sorted(self.settings_json['aliasMap'].keys())
+            sublime.active_window().show_quick_panel(self.aliasKeys, self.on_done)
+
+    def on_done(self, selected):
+        path = self.settings_json['aliasMap'][self.aliasKeys[selected]]
+        sublime.run_command("new_window")
+        d = {'folders': [{'follow_symlinks': True, 'path': path}]}
+        sublime.active_window().set_project_data(d)
