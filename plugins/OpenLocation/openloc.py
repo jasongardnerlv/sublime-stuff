ddce0de10b6ffc10e24f5227f267f6664ecd13cd
Jason Gardner <jason.gardner.lv@gmail.com>
2017-01-24 08:20:12 -0800

    handle file and folder locations in OpenLocation

--- plugins/OpenLocation/openloc.py
+++ plugins/OpenLocation/openloc.py
@@ -9,11 +9,17 @@ class OpenlocationPromptCommand(sublime_plugin.WindowCommand):
         with open(settings.get('settingsPath')) as json_data:
             self.settings_json = json.load(json_data)
             json_data.close()
-            self.aliasKeys = sorted(self.settings_json['aliasMap'].keys())
+            d = self.settings_json['folderAliases'].copy()
+            d.update(self.settings_json['fileAliases'])
+            self.aliasKeys = sorted(d.keys())
             sublime.active_window().show_quick_panel(self.aliasKeys, self.on_done)
 
     def on_done(self, selected):
-        path = self.settings_json['aliasMap'][self.aliasKeys[selected]]
-        sublime.run_command("new_window")
-        d = {'folders': [{'follow_symlinks': True, 'path': path}]}
-        sublime.active_window().set_project_data(d)
+        if self.aliasKeys[selected] in self.settings_json['folderAliases']:
+            path = self.settings_json['folderAliases'][self.aliasKeys[selected]]
+            sublime.run_command("new_window")
+            d = {'folders': [{'follow_symlinks': True, 'path': path}]}
+            sublime.active_window().set_project_data(d)
+        else:
+            path = self.settings_json['fileAliases'][self.aliasKeys[selected]]
+            self.window.open_file(path)
